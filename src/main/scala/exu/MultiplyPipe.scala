47f194bd2098c68d58a4f684e46935068b6dc521
miles-rusch-berkeley <miles.rusch@berkeley.edu>
2023-11-26 18:33:18 -0800

    edits to narrow mul bug

--- src/main/scala/exu/MultiplyPipe.scala
+++ src/main/scala/exu/MultiplyPipe.scala
@@ -56,66 +56,77 @@ class ElementwiseMultiplyPipe(depth: Int)(implicit p: Parameters) extends Pipeli
 }
 
 class SegmentedMultiplyPipe(depth: Int)(implicit p: Parameters) extends PipelinedFunctionalUnit(depth, false)(p) {
-
-  io.iss.sub_dlen := log2Ceil(dLenB).U - io.iss.op.rvs1_eew
+  io.iss.sub_dlen := 0.U
 
   lazy val ctrl_table = Seq(  
-      (OPMFunct6.mulhu,   Seq(N,N,N,N,N,X,Y)),
-      (OPMFunct6.mul,     Seq(N,Y,Y,N,N,X,N)),
-      (OPMFunct6.mulhsu,  Seq(N,N,Y,N,N,X,Y)),
-      (OPMFunct6.mulh,    Seq(N,Y,Y,N,N,X,Y)),
-      (OPMFunct6.madd,    Seq(N,Y,Y,Y,Y,N,N)),
-      (OPMFunct6.nmsub,   Seq(N,Y,Y,Y,Y,Y,N)),
-      (OPMFunct6.macc,    Seq(N,Y,Y,N,Y,N,N)),
-      (OPMFunct6.nmsac,   Seq(N,Y,Y,N,Y,Y,N)),
+      (OPMFunct6.mulhu,   Seq(N,N,N,N,X,Y)),
+      (OPMFunct6.mul,     Seq(Y,Y,N,N,X,N)),
+      (OPMFunct6.mulhsu,  Seq(N,Y,N,N,X,Y)),
+      (OPMFunct6.mulh,    Seq(Y,Y,N,N,X,Y)),
+      (OPMFunct6.madd,    Seq(Y,Y,Y,Y,N,N)),
+      (OPMFunct6.nmsub,   Seq(Y,Y,Y,Y,Y,N)),
+      (OPMFunct6.macc,    Seq(Y,Y,N,Y,N,N)),
+      (OPMFunct6.nmsac,   Seq(Y,Y,N,Y,Y,N)),
 
-      (OPMFunct6.wmulu,   Seq(Y,N,N,N,N,X,X)),
-      (OPMFunct6.wmulsu,  Seq(Y,N,Y,N,N,X,X)),
-      (OPMFunct6.wmul,    Seq(Y,Y,Y,N,N,X,X)),
-      (OPMFunct6.wmaccu,  Seq(Y,N,N,N,Y,N,X)),
-      (OPMFunct6.wmacc,   Seq(Y,Y,Y,N,Y,N,X)),
-      (OPMFunct6.wmaccus, Seq(Y,N,Y,N,Y,N,X)),
-      (OPMFunct6.wmaccsu, Seq(Y,Y,N,N,Y,N,X))
+      (OPMFunct6.wmulu,   Seq(N,N,N,N,X,X)),
+      (OPMFunct6.wmulsu,  Seq(N,Y,N,N,X,X)),
+      (OPMFunct6.wmul,    Seq(Y,Y,N,N,X,X)),
+      (OPMFunct6.wmaccu,  Seq(N,N,N,Y,N,X)),
+      (OPMFunct6.wmacc,   Seq(Y,Y,N,Y,N,X)),
+      (OPMFunct6.wmaccus, Seq(N,Y,N,Y,N,X)),
+      (OPMFunct6.wmaccsu, Seq(Y,N,N,Y,N,X))
   )
   override def accepts(f3: UInt, f6: UInt): Bool = VecDecode(f3, f6, ctrl_table.map(_._1))
-  val  ctrl_wide_in :: ctrl_rvs1_signed :: ctrl_rvs2_signed :: ctrl_madd :: ctrl_acc :: ctrl_sub :: ctrl_hi :: Nil = VecDecode.applyBools(
+  val ctrl_rvs1_signed :: ctrl_rvs2_signed :: ctrl_madd :: ctrl_acc :: ctrl_sub :: ctrl_hi :: Nil = VecDecode.applyBools(
     io.pipe(0).bits.funct3, io.pipe(0).bits.funct6,
-    Seq.fill(7)(BitPat.dontCare(1)), ctrl_table)
-
-  val wvd_eg = io.pipe(0).bits.wvd_eg(0)
+    Seq.fill(6)(BitPat.dontCare(1)), ctrl_table)
+  val ctrl_wide_in = io.pipe(0).bits.wvd_widen2
+  // val wvd_eg = io.pipe(0).bits.wvd_eg(0)
   val eew = io.pipe(0).bits.rvs1_eew // = to io.pipe(0).bits.rvs2_eew
   val in1 = io.pipe(0).bits.rvs1_data
   val in2 = Mux(ctrl_madd, io.pipe(0).bits.rvd_data, io.pipe(0).bits.rvs2_data)
   val ind = Mux(ctrl_madd, io.pipe(0).bits.rvs2_data, io.pipe(0).bits.rvd_data)
 
   val numSegMul = dLen/xLen
-  val viMul = Seq.fill(numSegMul) {Module(new SegmentedIntegerMultiplier(1))}
+  val vMul = Seq.fill(numSegMul) {Module(new SegmentedIntegerMultiplier(1))}
   for (i <- 0 until numSegMul) {
-    viMul(i).io.in1_signed := ctrl_rvs1_signed
-    viMul(i).io.in2_signed := ctrl_rvs2_signed
-    viMul(i).io.ctrl_acc := ctrl_acc
-    viMul(i).io.ctrl_madd := ctrl_madd
-    viMul(i).io.ctrl_sub := ctrl_sub
-    viMul(i).io.ctrl_wide_in := ctrl_wide_in
-    viMul(i).io.eew := eew
-    viMul(i).io.in1 := in1((i+1)*xLen-1, i*xLen)
-    viMul(i).io.in2 := in2((i+1)*xLen-1, i*xLen)
+    vMul(i).io.in1_signed := ctrl_rvs1_signed
+    vMul(i).io.in2_signed := ctrl_rvs2_signed
+    vMul(i).io.ctrl_acc := ctrl_acc
+    vMul(i).io.ctrl_madd := ctrl_madd
+    vMul(i).io.ctrl_sub := ctrl_sub
+    vMul(i).io.ctrl_wide_in := ctrl_wide_in
+    vMul(i).io.eew := eew
+    vMul(i).io.in1 := in1((i+1)*xLen-1, i*xLen)
+    vMul(i).io.in2 := in2((i+1)*xLen-1, i*xLen)
   }
-  val viMulOut = VecInit((0 until numSegMul).map { i => viMul(i).io.out_data }).asUInt
-  
-  val viAcc = Module(new SegmentedAdd)
-  viAcc.io.ctrl_sub := ctrl_sub
-  viAcc.io.eew := Mux(ctrl_wide_in, eew + 1.U, eew)
-  viAcc.io.in1 := Mux(ctrl_wide_in, viMulOut >> dLen, viMulOut)
-  viAcc.io.in2 := ind
-    // io.out_data := Mux(io.ctrl_acc, viAcc(i).io.out_data, mulOut)
-  val wide_out = Mux(ctrl_acc, viAcc.io.out, viMulOut)
-  io.write.valid     := io.pipe(0).valid
-  io.write.bits.eg   := io.pipe(0).bits.wvd_eg >> 1
-  io.write.bits.mask := Fill(2, FillInterleaved(8, io.pipe(0).bits.wmask))
-  io.write.bits.data := Fill(2, wide_out)
+  val vWideMulOut = VecInit((0 until numSegMul).map { i => vMul(i).io.out_data }).asUInt
+  val vNarrowMulOutEew = Wire(Vec(4, UInt(dLen.W)))
+  for (eew <- 0 until 4) {
+    val wide = vWideMulOut.asTypeOf(Vec(dLenB >> eew, UInt((16 << eew).W)))
+    vNarrowMulOutEew(eew) := wide.map { i => 
+      val lo = wide(i)((8 << eew)-1, 0)
+      val hi = wide(i)((16 << eew)-1, (8 << eew))
+      Mux(ctrl_hi, hi, lo)
+    }.asUInt
+  }
+  val vNarrowMulOut = vNarrowMulOutEew(eew)
+  val vAcc = Module(new SegmentedAdd)
+  vAcc.io.ctrl_sub := ctrl_sub
+  vAcc.io.eew := Mux(ctrl_wide_in, eew + 1.U, eew)
+  vAcc.io.in1 := Mux(ctrl_wide_in, vWideMulOut >> dLen, vWideMulOut)
+  vAcc.io.in2 := ind
 
-  when (io.pipe(0).bits.wvd_widen2 && ~ctrl_acc) {
-    io.write.bits.data := wide_out
-  }
+  val narrow_out = Mux(ctrl_acc, vAcc.io.out, vNarrowMulOut)
+  val wide_out = vWideMulOut
+  val out = Pipe(
+    io.pipe(0).valid, 
+    Mux(ctrl_wide_in && ~ctrl_acc, wide_out, Fill(2, narrow_out)), 
+    depth-1
+  ).bits
+  
+  io.write.valid     := io.pipe(depth-1).valid
+  io.write.bits.eg   := io.pipe(depth-1).bits.wvd_eg
+  io.write.bits.mask := Fill(2, FillInterleaved(8, io.pipe(0).bits.wmask))
+  io.write.bits.data := out
 }
