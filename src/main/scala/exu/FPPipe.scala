65535252930e49bdd10c10a61a98accaacaa6a4f
dpgrubb13 <dpgrubb@berkeley.edu>
2023-12-09 11:43:35 -0800

    clean up FPPipe control signals, add more ops

--- src/main/scala/exu/FPPipe.scala
+++ src/main/scala/exu/FPPipe.scala
@@ -64,51 +64,69 @@ class FPPipe(implicit p: Parameters) extends PipelinedFunctionalUnit(3, true)(p)
   io.iss.sub_dlen := 0.U
   io.set_vxsat := false.B
 
-  override def accepts(f3: UInt, f6: UInt): Bool = f3.isOneOf(OPFVV, OPFVF)
-
-  val default = List(X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X,X)
-
-  val useOne :: ldst :: wen :: ren1 :: ren2 :: ren3 :: swap12 :: swap23 :: fromint :: toint :: fastpipe :: fma :: fmaCmd0 :: fmaCmd1 :: div :: sqrt :: wflags :: Nil = VecDecode.applyBools(
-    io.pipe(0).bits.funct3, io.pipe(0).bits.funct6,
-    default, Seq(
-      (OPFFunct6.vfadd,   Seq(Y,N,N,Y,Y,Y,N,N,N,N,N,Y,N,N,N,N,N)),
-      (OPFFunct6.vfsub,   Seq(Y,N,N,Y,Y,Y,N,N,N,N,N,Y,Y,N,N,N,N)),
-      (OPFFunct6.vfmacc,  Seq(N,N,N,Y,Y,Y,N,N,N,N,N,Y,N,N,N,N,Y)),
-      (OPFFunct6.vfnmacc, Seq(N,N,N,Y,Y,Y,N,N,N,N,N,Y,Y,Y,N,N,Y)),
-      (OPFFunct6.vfmsac,  Seq(N,N,N,Y,Y,Y,N,N,N,N,N,Y,Y,N,N,N,Y)),
-      (OPFFunct6.vfnmsac, Seq(N,N,N,Y,Y,Y,N,N,N,N,N,Y,N,Y,N,N,Y)),
-    ))  
-
   val is_opfvf = io.pipe(0).bits.funct3.isOneOf(OPFVF)
-
-  val one_d = "h3FF0000000000000".U
-  val one_s = "h3F800000".U
   val frs1_data = io.pipe(0).bits.frs1_data
-
-  val fmaCmd = Cat(fmaCmd1, fmaCmd0)
-
   val one_bits = Mux(io.pipe(0).bits.vd_eew === 3.U, "h3FF0000000000000".U, "h3F8000003F800000".U)
   val scalar_operand_bits = Mux(io.pipe(0).bits.vd_eew === 3.U, frs1_data, Fill(2, frs1_data))
-
   val vec_rvs1 = io.pipe(0).bits.rvs1_data.asTypeOf(Vec(fmaCount, UInt(64.W)))
   val vec_rvs2 = io.pipe(0).bits.rvs2_data.asTypeOf(Vec(fmaCount, UInt(64.W)))
   val vec_rvd = io.pipe(0).bits.rvd_data.asTypeOf(Vec(fmaCount, UInt(64.W)))
+
+  lazy val ctrl_table = Seq(
+    (OPFFunct6.vfadd,     Seq(Y,N,N,N,N)),
+    (OPFFunct6.vfsub,     Seq(Y,N,N,Y,N)),
+    (OPFFunct6.vfrsub,    Seq(Y,N,N,N,Y)),
+    (OPFFunct6.vfmul,     Seq(N,Y,N,N,N)),
+    (OPFFunct6.vfmacc,    Seq(Y,Y,N,N,N)),
+    (OPFFunct6.vfnmacc,   Seq(Y,Y,N,Y,Y)),
+    (OPFFunct6.vfmsac,    Seq(Y,Y,N,Y,N)),
+    (OPFFunct6.vfnmsac,   Seq(Y,Y,N,N,Y)),
+    (OPFFunct6.vfmadd,    Seq(Y,Y,Y,N,N)),
+    (OPFFunct6.vfnmadd,   Seq(Y,Y,Y,Y,Y)),
+    (OPFFunct6.vfmsub,    Seq(Y,Y,Y,Y,N)),
+    (OPFFunct6.vfnmsub,   Seq(Y,Y,Y,N,Y)),
+  )
+  override def accepts(f3: UInt, f6: UInt): Bool = f3.isOneOf(OPFVV, OPFVF)
+  val ctrl_add :: ctrl_fma :: ctrl_swap23 :: ctrl_fmaCmd0 :: ctrl_fmaCmd1 :: Nil = VecDecode.applyBools(
+    io.pipe(0).bits.funct3, io.pipe(0).bits.funct6,
+    Seq.fill(5)(X), ctrl_table
+  )
+
+  val fmaCmd = Cat(ctrl_fmaCmd1, ctrl_fmaCmd0)
 
   val ieee_out = Wire(Vec(fmaCount, UInt(64.W)))
   val exc = Wire(Vec(fmaCount, UInt(5.W))) 
 
   val fma_pipes = Seq.fill(fmaCount)(Module(new TandemFMAPipe(depth))).zipWithIndex.map { case(fma_pipe, i) =>
-    val multiplier_bits = Mux(useOne, one_bits, Mux(is_opfvf, scalar_operand_bits, vec_rvs1(i)))
-    val multiplicand_bits = vec_rvs2(i)
-    val addend_bits = Mux(useOne, vec_rvs1(i), vec_rvd(i))
+    val rs1_bits = Mux(is_opfvf, scalar_operand_bits, vec_rvs1(i))
+
+    when (ctrl_fma && ctrl_add) {
+      fma_pipe.io.a := rs1_bits
+      when (ctrl_swap23) {
+        fma_pipe.io.b := vec_rvd(i)
+        fma_pipe.io.c := vec_rvs2(i)
+      } .otherwise {
+        fma_pipe.io.b := vec_rvs2(i)
+        fma_pipe.io.c := vec_rvd(i)
+      }
+    } .elsewhen (ctrl_fma) {
+      fma_pipe.io.a := rs1_bits
+      fma_pipe.io.b := vec_rvs2(i)
+      fma_pipe.io.c := 0.U
+    } .elsewhen (ctrl_add) {
+      fma_pipe.io.a := one_bits
+      fma_pipe.io.b := vec_rvs2(i)
+      fma_pipe.io.c := rs1_bits
+    } .otherwise {
+      fma_pipe.io.a := 0.U
+      fma_pipe.io.b := 0.U
+      fma_pipe.io.c := 0.U
+    }
     
     fma_pipe.io.valid := io.pipe(0).valid
     fma_pipe.io.frm := io.pipe(0).bits.frm
     fma_pipe.io.op := fmaCmd
     fma_pipe.io.eew := io.pipe(0).bits.vd_eew
-    fma_pipe.io.a := multiplicand_bits
-    fma_pipe.io.b := multiplier_bits
-    fma_pipe.io.c := addend_bits
     ieee_out(i) := fma_pipe.io.out
     exc(i) := fma_pipe.io.exc
     fma_pipe
