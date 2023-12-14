16ea08cb12d6ef6b549bf3fb00ad1f54eeef7377
dpgrubb13 <dpgrubb@berkeley.edu>
2023-12-13 19:48:43 -0800

    min and max working

--- src/main/scala/backend/Backend.scala
+++ src/main/scala/backend/Backend.scala
@@ -91,7 +91,8 @@ class VectorBackend(implicit p: Parameters) extends CoreModule()(p) with HasVect
     () => new IntegerPipe,
     () => if (vParams.useSegmentedIMul) (new SegmentedMultiplyPipe(3)) else (new ElementwiseMultiplyPipe(3)),
     () => new IterativeIntegerDivider,
-    () => new FMAPipe(vParams.fmaPipeDepth)
+    () => new FMAPipe(vParams.fmaPipeDepth),
+    () => new FPCompPipe
   )))
 
   vdq.io.deq.ready := seqs.map(_.io.dis.ready).andR
