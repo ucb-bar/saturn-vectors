75f279dba2c537aafd807d7418046c4228ff0b58
Ella Schwarz <schwarzem@a6.Millennium.Berkeley.EDU>
2024-01-22 21:16:57 -0800

    Use BuildVectorUnit with selective build

--- src/main/scala/rocket/Configs.scala
+++ src/main/scala/rocket/Configs.scala
@@ -8,7 +8,7 @@ import freechips.rocketchip.tile._
 import freechips.rocketchip.diplomacy._
 import vector.common._
 
-class WithRocketVectorUnit(vLen: Int = 128, dLen: Int = 64, params: VectorParams = VectorParams()) extends Config((site, here, up) => {
+class WithRocketVectorUnit(vLen: Int = 128, dLen: Int = 64, params: VectorParams = VectorParams(), cores: Option[Seq[Int]] = None) extends Config((site, here, up) => {
   case BuildVectorUnit => Some((p: Parameters) => {
     val vec = LazyModule(new VectorUnit()(p.alterPartial {
       case VectorParamsKey => params.copy(dLen=dLen)
@@ -16,16 +16,20 @@ class WithRocketVectorUnit(vLen: Int = 128, dLen: Int = 64, params: VectorParams
     vec
   })
   case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
-    case tp: RocketTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(
-      core = tp.tileParams.core.copy(vector = Some(RocketCoreVectorParams(
-        vLen = vLen,
-        vMemDataBits = dLen,
-        decoder = ((p: Parameters) => {
-          val decoder = Module(new EarlyVectorDecode()(p))
-          decoder
-        })
-      ))),
-      dcache = tp.tileParams.dcache.map(_.copy(rowBits = dLen))))
+    case tp: RocketTileAttachParams => {
+      val buildVector = cores.map(_.contains(tp.tileParams.hartId)).getOrElse(true)
+      if (buildVector) tp.copy(tileParams = tp.tileParams.copy(
+        core = tp.tileParams.core.copy(vector = Some(RocketCoreVectorParams(
+          vLen = vLen,
+          vMemDataBits = dLen,
+          decoder = ((p: Parameters) => {
+            val decoder = Module(new EarlyVectorDecode()(p))
+            decoder
+          })
+        ))),
+        dcache = tp.tileParams.dcache.map(_.copy(rowBits = dLen)))
+      ) else tp
+    }
     case other => other
   }
 })
