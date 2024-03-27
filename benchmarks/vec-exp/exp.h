d65124dd965787a1ac525ed33863704b56c4611b
Jerry Zhao <jerryz123@berkeley.edu>
2024-03-27 15:54:25 -0700

    Optimize vec-exp a bit

--- benchmarks/vec-exp/exp.h
+++ benchmarks/vec-exp/exp.h
@@ -25,68 +25,68 @@
 #define EXP_IMPL(t,w,l,b,c1,c2)						\
 void exp_f##w##m##l##_bmark(t *exponents, t *results, size_t len);	        \
 inline vfloat##w##m##l##_t __exp_f##w##m##l(vfloat##w##m##l##_t x, size_t gvl) {	\
-   vfloat##w##m##l##_t exp_hi = __riscv_vfmv_v_f_f##w##m##l(88.3762626##w##7949, gvl); \
-   vfloat##w##m##l##_t exp_lo = __riscv_vfmv_v_f_f##w##m##l(-88.3762626##w##7949, gvl); \
+  t exp_hi = 88.3762626##w##7949;					\
+  t exp_lo = -88.3762626##w##7949;					\
 									\
-   vfloat##w##m##l##_t cephes_LOG2EF = __riscv_vfmv_v_f_f##w##m##l(1.44269504088896341, gvl); \
-   vfloat##w##m##l##_t cephes_exp_C1 = __riscv_vfmv_v_f_f##w##m##l(0.693359375, gvl); \
-   vfloat##w##m##l##_t cephes_exp_C2 = __riscv_vfmv_v_f_f##w##m##l(-2.12194440e-4, gvl); \
+  t cephes_LOG2EF = 1.44269504088896341;				\
+  t cephes_exp_C1 = 0.693359375;					\
+  t cephes_exp_C2 = -2.12194440e-4;					\
+  									\
+  t cephes_exp_p0 = 1.9875691500E-4;					\
+  t cephes_exp_p1 = 1.3981999507E-3;					\
+  t cephes_exp_p2 = 8.3334519073E-3;					\
+  t cephes_exp_p3 = 4.1665795894E-2;					\
+  t cephes_exp_p4 = 1.6666665459E-1;					\
+  t cephes_exp_p5 = 5.0000001201E-1;					\
+  vfloat##w##m##l##_t tmp;						\
+  vfloat##w##m##l##_t tmp2;						\
+  vfloat##w##m##l##_t tmp4;						\
+  vfloat##w##m##l##_t fx;						\
    									\
-   vfloat##w##m##l##_t cephes_exp_p0 = __riscv_vfmv_v_f_f##w##m##l(1.9875691500E-4, gvl); \
-   vfloat##w##m##l##_t cephes_exp_p1 = __riscv_vfmv_v_f_f##w##m##l(1.3981999507E-3, gvl); \
-   vfloat##w##m##l##_t cephes_exp_p2 = __riscv_vfmv_v_f_f##w##m##l(8.3334519073E-3, gvl); \
-   vfloat##w##m##l##_t cephes_exp_p3 = __riscv_vfmv_v_f_f##w##m##l(4.1665795894E-2, gvl); \
-   vfloat##w##m##l##_t cephes_exp_p4 = __riscv_vfmv_v_f_f##w##m##l(1.6666665459E-1, gvl); \
-   vfloat##w##m##l##_t cephes_exp_p5 = __riscv_vfmv_v_f_f##w##m##l(5.0000001201E-1, gvl); \
-   vfloat##w##m##l##_t tmp;							\
-   vfloat##w##m##l##_t tmp2;						\
-   vfloat##w##m##l##_t tmp4;						\
-   vfloat##w##m##l##_t fx;							\
-   									\
-   vfloat##w##m##l##_t one = __riscv_vfmv_v_f_f##w##m##l(1.0, gvl);		\
-   vfloat##w##m##l##_t zero = __riscv_vfmv_v_f_f##w##m##l(0.0, gvl);		\
-   vfloat##w##m##l##_t z;							\
-   vfloat##w##m##l##_t y;							\
-   									\
-   vbool##b##_t mask;							\
-   vint##w##m##l##_t imm0;							\
-   vint##w##m##l##_t tmp3;							\
+  vfloat##w##m##l##_t one = __riscv_vfmv_v_f_f##w##m##l(1.0, gvl);	\
+  vfloat##w##m##l##_t zero = __riscv_vfmv_v_f_f##w##m##l(0.0, gvl);	\
+  vfloat##w##m##l##_t z;						\
+  vfloat##w##m##l##_t y;						\
+  									\
+  vbool##b##_t mask;							\
+  vint##w##m##l##_t imm0;						\
+  vint##w##m##l##_t tmp3;						\
+  									\
+  x = __riscv_vfmin_vf_f##w##m##l(x, exp_hi, gvl);			\
+  x = __riscv_vfmax_vf_f##w##m##l(x, exp_lo, gvl);			\
+  									\
+  fx = __riscv_vfmv_v_f_f##w##m##l(0.5, gvl);				\
+  fx = __riscv_vfmacc_vf_f##w##m##l(fx, cephes_LOG2EF, x, gvl);	\
 									\
-   x = __riscv_vfmin_vv_f##w##m##l(x, exp_hi, gvl);			\
-   x = __riscv_vfmax_vv_f##w##m##l(x, exp_lo, gvl);			\
+  tmp3 = __riscv_vfcvt_x_f_v_i##w##m##l(fx, gvl);			\
+  tmp = __riscv_vfcvt_f_x_v_f##w##m##l(tmp3, gvl);			\
 									\
-   fx = __riscv_vfmv_v_f_f##w##m##l(0.5, gvl);				\
-   fx = __riscv_vfmacc_vv_f##w##m##l(fx, x, cephes_LOG2EF, gvl);		\
+  mask = __riscv_vmflt_vv_f##w##m##l##_b##b(fx, tmp, gvl);		\
+  tmp2 = __riscv_vmerge_vvm_f##w##m##l(zero, one, mask, gvl);		\
+  fx = __riscv_vfsub_vv_f##w##m##l(tmp, tmp2, gvl);			\
+  tmp = __riscv_vfmul_vf_f##w##m##l(fx, cephes_exp_C1, gvl);		\
+  z = __riscv_vfmul_vf_f##w##m##l(fx, cephes_exp_C2, gvl);		\
+  x = __riscv_vfsub_vv_f##w##m##l(x, tmp, gvl);				\
+  x = __riscv_vfsub_vv_f##w##m##l(x, z, gvl);				\
+  									\
+  z = __riscv_vfmul_vv_f##w##m##l(x, x, gvl);				\
+  									\
+  y = __riscv_vfmv_v_f_f##w##m##l(cephes_exp_p0, gvl);			\
+  y = __riscv_vfmadd_vf_f##w##m##l(y, cephes_exp_p1, x, gvl);		\
+  y = __riscv_vfmadd_vf_f##w##m##l(y, cephes_exp_p2, x, gvl);		\
+  y = __riscv_vfmadd_vf_f##w##m##l(y, cephes_exp_p3, x, gvl);		\
+  y = __riscv_vfmadd_vf_f##w##m##l(y, cephes_exp_p4, x, gvl);		\
+  y = __riscv_vfmadd_vf_f##w##m##l(y, cephes_exp_p5, x, gvl);		\
+  y = __riscv_vfmadd_vv_f##w##m##l(y, z, x, gvl);			\
+  y = __riscv_vfadd_vv_f##w##m##l(y, one, gvl);				\
 									\
-   tmp3 = __riscv_vfcvt_x_f_v_i##w##m##l(fx, gvl);			\
-   tmp = __riscv_vfcvt_f_x_v_f##w##m##l(tmp3, gvl);			\
-									\
-   mask = __riscv_vmflt_vv_f##w##m##l##_b##b(fx, tmp, gvl);			\
-   tmp2 = __riscv_vmerge_vvm_f##w##m##l(zero, one, mask, gvl);		\
-   fx = __riscv_vfsub_vv_f##w##m##l(tmp, tmp2, gvl);			\
-   tmp = __riscv_vfmul_vv_f##w##m##l(fx, cephes_exp_C1, gvl);		\
-   z = __riscv_vfmul_vv_f##w##m##l(fx, cephes_exp_C2, gvl);		\
-   x = __riscv_vfsub_vv_f##w##m##l(x, tmp, gvl);				\
-   x = __riscv_vfsub_vv_f##w##m##l(x, z, gvl);				\
-									\
-   z = __riscv_vfmul_vv_f##w##m##l(x, x, gvl);				\
-									\
-   y = cephes_exp_p0;							\
-   y = __riscv_vfmadd_vv_f##w##m##l(y, x, cephes_exp_p1, gvl);		\
-   y = __riscv_vfmadd_vv_f##w##m##l(y, x, cephes_exp_p2, gvl);		\
-   y = __riscv_vfmadd_vv_f##w##m##l(y, x, cephes_exp_p3, gvl);		\
-   y = __riscv_vfmadd_vv_f##w##m##l(y, x, cephes_exp_p4, gvl);		\
-   y = __riscv_vfmadd_vv_f##w##m##l(y, x, cephes_exp_p5, gvl);		\
-   y = __riscv_vfmadd_vv_f##w##m##l(y, z, x, gvl);			\
-   y = __riscv_vfadd_vv_f##w##m##l(y, one, gvl);				\
-									\
-   imm0 = __riscv_vfcvt_x_f_v_i##w##m##l(fx, gvl);			\
-   imm0 = __riscv_vadd_vv_i##w##m##l(imm0, __riscv_vmv_v_x_i##w##m##l(c1, gvl), gvl); \
-   imm0 = __riscv_vsll_vv_i##w##m##l(imm0, __riscv_vmv_v_x_u##w##m##l(c2, gvl), gvl); \
-									\
-   tmp4 = __riscv_vreinterpret_v_i##w##m##l##_f##w##m##l(imm0);		\
-   y = __riscv_vfmul_vv_f##w##m##l(y, tmp4, gvl);				\
-   return y;								\
+  imm0 = __riscv_vfcvt_x_f_v_i##w##m##l(fx, gvl);			\
+  imm0 = __riscv_vadd_vv_i##w##m##l(imm0, __riscv_vmv_v_x_i##w##m##l(c1, gvl), gvl); \
+  imm0 = __riscv_vsll_vv_i##w##m##l(imm0, __riscv_vmv_v_x_u##w##m##l(c2, gvl), gvl); \
+  									\
+  tmp4 = __riscv_vreinterpret_v_i##w##m##l##_f##w##m##l(imm0);		\
+  y = __riscv_vfmul_vv_f##w##m##l(y, tmp4, gvl);			\
+  return y;								\
 }
 
 EXP_IMPL(double,64,1,64,1023,52);
