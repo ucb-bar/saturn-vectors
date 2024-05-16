d064067950760b2c50129d7dd539f949060d0ea1
dpgrubb13 <dpgrubb@berkeley.edu>
2024-05-15 17:18:34 -0700

    remove comment

--- src/main/scala/backend/Backend.scala
+++ src/main/scala/backend/Backend.scala
@@ -378,7 +378,7 @@ class VectorBackend(implicit p: Parameters) extends CoreModule()(p) with HasVect
     val first_ready_seq = PriorityEncoder(group.seqs.map(_.io.dis.ready))
 
     group.seqs.zipWithIndex.foreach{ case(s, j) =>
-      s.io.dis.valid := group.issq.io.deq.valid && (first_ready_seq === j.U) //&& group.issq.io.deq.bits.seq(j)
+      s.io.dis.valid := group.issq.io.deq.valid && (first_ready_seq === j.U)
       s.io.dis.bits := group.issq.io.deq.bits.viewAsSupertype(new BackendIssueInst)
     }
     group.issq.io.deq.ready := group.seqs.map(_.io.dis.ready).reduce(_ || _)
