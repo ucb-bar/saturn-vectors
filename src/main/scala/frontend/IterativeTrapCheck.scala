75acfd37a220c6b155b6c6d1151ff896b8d5434f
Jerry Zhao <jerryz123@berkeley.edu>
2024-05-16 17:14:07 -0700

    Cut index access critical path

--- src/main/scala/frontend/IterativeTrapCheck.scala
+++ src/main/scala/frontend/IterativeTrapCheck.scala
@@ -11,6 +11,75 @@ import freechips.rocketchip.diplomacy._
 
 import saturn.common._
 
+class IndexMaskAccess(implicit p: Parameters) extends CoreModule()(p) with HasVectorParams {
+  val io = IO(new Bundle {
+    val in = Input(Bool())
+    val inst = Input(new VectorIssueInst)
+
+    val index_access = Flipped(new VectorIndexAccessIO)
+    val mask_access = Flipped(new VectorMaskAccessIO)
+
+    val access = new Bundle {
+      val ready = Output(Bool())
+      val eidx = Input(UInt(log2Ceil(maxVLMax).W))
+      val index = Output(UInt(64.W))
+      val mask = Output(Bool())
+    }
+
+    val pop = Input(Valid(UInt(log2Ceil(maxVLMax).W)))
+    val flush = Input(Bool())
+  })
+
+  val valid = RegInit(false.B)
+  val eidx = Reg(UInt(log2Ceil(maxVLMax).W))
+  // This all works only with pow2 buffers and eidx starting at 0
+  val valids = Reg(Vec(4, Bool()))
+  val indices = Reg(Vec(4, UInt(64.W)))
+  val masks = Reg(Vec(4, Bool()))
+  when (io.in) {
+    assert(!valid)
+    valid := true.B
+    eidx := 0.U
+    valids.foreach(_ := false.B)
+  }
+
+  val needs_index = io.inst.mop.isOneOf(mopOrdered, mopUnordered)
+  val needs_mask = !io.inst.vm
+
+  val index_ready = io.index_access.ready || !needs_index
+  val mask_ready = io.mask_access.ready || !needs_mask
+
+  io.index_access.valid := valid && needs_index && !valids(eidx(1,0))
+  io.mask_access.valid  := valid && needs_mask && !valids(eidx(1,0))
+
+  io.index_access.vrs  := io.inst.rs2
+  io.index_access.eidx := eidx
+  io.index_access.eew  := io.inst.mem_idx_size
+  io.mask_access.eidx := eidx
+
+  when (valid && index_ready && mask_ready && !valids(eidx(1,0))) {
+    val next_eidx = eidx +& 1.U
+    eidx := eidx + 1.U
+    when (next_eidx === io.inst.vconfig.vl) {
+      valid := false.B
+    }
+    valids(eidx(1,0)) := true.B
+    indices(eidx(1,0)) := io.index_access.idx
+    masks(eidx(1,0)) := io.mask_access.mask
+  }
+
+  io.access.ready := valids(io.access.eidx(1,0))
+  io.access.index := indices(io.access.eidx(1,0))
+  io.access.mask  := masks(io.access.eidx(1,0))
+
+  when (io.pop.fire) {
+    valids(io.pop.bits(1,0)) := false.B
+  }
+  when (io.flush) {
+    valid := false.B
+  }
+}
+
 class IterativeTrapCheck(implicit p: Parameters) extends CoreModule()(p) with HasVectorParams {
   val io = IO(new Bundle {
     val status = Input(new MStatus)
@@ -24,7 +93,7 @@ class IterativeTrapCheck(implicit p: Parameters) extends CoreModule()(p) with Ha
     val vstart     = Valid(UInt(log2Ceil(maxVLMax).W))
     val vconfig    = Valid(new VConfig)
     val xcpt       = Valid(new Bundle {
-      val cause = UInt(xLen.W)
+       val cause = UInt(xLen.W)
       val tval = UInt(coreMaxAddrBits.W)
     })
     val inst       = Output(new VectorIssueInst)
@@ -46,6 +115,12 @@ class IterativeTrapCheck(implicit p: Parameters) extends CoreModule()(p) with Ha
   val tlb_backoff = RegInit(0.U(2.W))
   when (tlb_backoff =/= 0.U) { tlb_backoff := tlb_backoff - 1.U }
 
+  val im_access = Module(new IndexMaskAccess)
+  im_access.io.in := io.in.valid
+  im_access.io.inst := inst
+  im_access.io.index_access <> io.index_access
+  im_access.io.mask_access <> io.mask_access
+
   when (io.in.valid) {
     assert(!valid)
     valid := true.B
@@ -60,30 +135,26 @@ class IterativeTrapCheck(implicit p: Parameters) extends CoreModule()(p) with Ha
     (mopUnit    -> ((inst.seg_nf +& 1.U) << inst.mem_elem_size)),
     (mopStrided -> inst.rs2_data)
   ))
+
   val indexed = inst.mop.isOneOf(mopOrdered, mopUnordered)
-  val index_ready = !indexed || io.index_access.ready
-  val mask_ready = inst.vm || io.mask_access.ready
-  val index = Mux(indexed, io.index_access.idx & eewBitMask(inst.mem_idx_size), 0.U)
+  val index_ready = !indexed || im_access.io.access.ready
+  val mask_ready = inst.vm || im_access.io.access.ready
+  val index = Mux(indexed, im_access.io.access.index & eewBitMask(inst.mem_idx_size), 0.U)
   val base = Mux(indexed, inst.rs1_data, addr)
   val indexaddr = base + index
   val tlb_addr = Mux(seg_hi, nextPage(indexaddr), indexaddr)
   val seg_nf_consumed = ((1 << pgIdxBits).U - Mux(seg_hi, indexaddr, tlb_addr)(pgIdxBits-1,0)) >> inst.mem_elem_size
   val seg_single_page = seg_nf_consumed >= (inst.seg_nf +& 1.U)
-  val masked = !io.mask_access.mask && !inst.vm
+  val masked = !im_access.io.access.mask && !inst.vm
   val tlb_valid = eidx < inst.vconfig.vl && eidx >= inst.vstart && !masked
   val ff = inst.umop === lumopFF && inst.mop === mopUnit
 
   io.busy := valid
   io.inst := inst
-  io.index_access.valid := valid && indexed && tlb_backoff === 0.U
-  io.index_access.vrs   := inst.rs2
-  io.index_access.eidx  := eidx
-  io.index_access.eew   := inst.mem_idx_size
 
-  io.mask_access.valid := valid && !inst.vm && tlb_backoff === 0.U
-  io.mask_access.eidx  := eidx
+  im_access.io.access.eidx := eidx
 
-  io.s0_tlb_req.valid            := tlb_valid && tlb_backoff === 0.U && mask_ready && index_ready
+  io.s0_tlb_req.valid            := tlb_valid && tlb_backoff === 0.U && index_ready && mask_ready
   io.s0_tlb_req.bits.vaddr       := tlb_addr
   io.s0_tlb_req.bits.passthrough := false.B
   io.s0_tlb_req.bits.size        := inst.mem_elem_size
@@ -153,6 +224,9 @@ class IterativeTrapCheck(implicit p: Parameters) extends CoreModule()(p) with Ha
   io.vconfig.valid := false.B
   io.vconfig.bits := inst.vconfig
   io.vconfig.bits.vl := s1_eidx
+  im_access.io.pop.valid := false.B
+  im_access.io.pop.bits := s1_eidx
+  im_access.io.flush := false.B
 
   when (s1_valid) {
     io.issue.valid := !tlb_resp.miss && !xcpt && s1_eidx >= inst.vstart && !s1_masked
@@ -164,12 +238,17 @@ class IterativeTrapCheck(implicit p: Parameters) extends CoreModule()(p) with Ha
       }
     }
 
+    when (s1_seg_hi || s1_seg_single_page || inst.seg_nf === 0.U) {
+      im_access.io.pop.valid := true.B
+    }
+
     when (tlb_resp.miss || !io.issue.ready) {
       tlb_backoff := 3.U
       replay_kill := true.B
       eidx := s1_eidx
       addr := s1_base
       seg_hi := s1_seg_hi
+      im_access.io.pop.valid := false.B
     } .elsewhen (xcpt) {
       val ff_nofault = ff && s1_eidx =/= 0.U
       valid := false.B
@@ -178,12 +257,14 @@ class IterativeTrapCheck(implicit p: Parameters) extends CoreModule()(p) with Ha
       io.xcpt.valid := !ff_nofault
       io.vstart.valid := !ff_nofault
       io.vconfig.valid := ff_nofault
+      im_access.io.flush := true.B
     } .elsewhen ((s1_eidx +& 1.U) === inst.vconfig.vl && (s1_seg_hi || s1_seg_single_page || inst.seg_nf === 0.U)) {
       valid := false.B
       replay_kill := true.B
       io.retire := true.B
       io.vstart.valid := true.B
       io.vstart.bits := 0.U
+      im_access.io.flush := true.B
     }
   }
 }
