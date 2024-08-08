73d16239cca0f8d0c6640c74d7be540142acfb7d
Jerry Zhao <jerryz123@berkeley.edu>
2024-08-08 13:53:45 -0700

    Fix fft2 bmark

--- benchmarks/vec-fft/fft2_main.c
+++ benchmarks/vec-fft/fft2_main.c
@@ -19,9 +19,15 @@
 int main( int argc, char* argv[] )
 {
 #if PREALLOCATE
-  fft2(input_Xr, input_Xi, input_Wr, input_Wi, DATA_SIZE, LOG2_DATA_SIZE);
+  for (size_t i = 0; i < DATA_SIZE-1; i++) {
+    volatile float tmp;
+    tmp = input_Xr[i];
+    tmp = input_Xi[i];
+    tmp = input_Wr[i];
+    tmp = input_Wi[i];
+  }
 #endif
-  
+
   // Do the FFT
   setStats(1);
   fft2(input_Xr, input_Xi, input_Wr, input_Wi, DATA_SIZE, LOG2_DATA_SIZE);
