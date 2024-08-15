66380c162d929317f78b1d519615678667027179
Jerry Zhao <jerryz123@berkeley.edu>
2024-08-14 17:39:34 -0700

    Don't build fredusum test

--- build-tests.sh
+++ build-tests.sh
@@ -6,6 +6,7 @@ cd riscv-vector-tests
 
 make MODE=machine VLEN=256 XLEN=64 SPLIT=50000 TEST_MODE="cosim" generate-stage1
 make MODE=machine VLEN=256 XLEN=64 SPLIT=50000 TEST_MODE="cosim" all -j72
+rm -rf out/v256x64machine/bin/stage2/vfredusum*
 
 make MODE=virtual VLEN=256 XLEN=64 SPLIT=6000 TEST_MODE="cosim" PATTERN='^v[ls].+\.v$' generate-stage1
 make MODE=virtual VLEN=256 XLEN=64 SPLIT=6000 TEST_MODE="cosim" PATTERN='^v[ls].+\.v$' all -j72
