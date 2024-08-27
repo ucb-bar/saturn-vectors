2lambda123/ucb-bar-saturn-vectors

Add support for the o3 pipeline visualizer

src/main/scala/mem/LoadOrderBuffer.scala
@@ -102,6 +102,9 @@ class LoadOrderBuffer(nEntries: Int, nRobEntries: Int)(implicit p: Parameters) e
   io.replay.bits.tag  := next_replay
   io.replay.bits.store := false.B
 
+  io.replay.bits.debug_id := DontCare
+  io.replay.bits.debug_eg := DontCare
+
   when (io.replay.fire) {
     must_replay(next_replay) := false.B
   }

ucb-bar/saturn-vectors

src/main/scala/mem/LoadOrderBuffer.scala
@@ -102,6 +102,10 @@ class LoadOrderBuffer(nEntries: Int, nRobEntries: Int)(implicit p: Parameters) e
   io.replay.bits.tag  := next_replay
   io.replay.bits.store := false.B
 
+  io.replay.bits.debug_id := DontCare
+  io.replay.bits.debug_eidx := DontCare
+  io.replay.bits.debug_nelems := DontCare
+
   when (io.replay.fire) {
     must_replay(next_replay) := false.B
   }