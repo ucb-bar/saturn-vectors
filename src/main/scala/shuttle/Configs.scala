f8ecec7d0a24a50a7460ec34fbf9e0d597625e2f
Kevin Anderson <kevinand@bwrcrdsl-1.eecs.berkeley.edu>
2025-04-28 18:53:21 -0700

    fixup; OPU integreation

--- src/main/scala/shuttle/Configs.scala
+++ src/main/scala/shuttle/Configs.scala
@@ -16,7 +16,8 @@ class WithShuttleVectorUnit(
   params: VectorParams = VectorParams(),
   cores: Option[Seq[Int]] = None,
   location: HierarchicalLocation = InSubsystem,
-  mLen: Option[Int] = None
+  mLen: Option[Int] = None,
+  add_opu : Boolean = false
 ) extends Config((site, here, up) => {
   case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
     case tp: ShuttleTileAttachParams => {
@@ -25,6 +26,7 @@ class WithShuttleVectorUnit(
         dLen=dLen,
         mLen=mLen.getOrElse(dLen),
         useScalarFPFMA = false,
+        add_opu = add_opu,
       )
       if (buildVector) tp.copy(tileParams = tp.tileParams.copy(
         core = tp.tileParams.core.copy(
